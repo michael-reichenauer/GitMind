b0e08f4d978d6c47d39909ce9b1d217b99c2d326
Michael Reichenauer <michael.reichenauer@gmail.com>
2016-04-04 20:37:41 +0200

    Improved BuilSetup and now produces a version file

--- BuildSetup.bat
+++ BuildSetup.bat
@@ -1,7 +1,20 @@
 @echo off
-"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
+echo Builing setup ...
+echo.
 
-mkdir Releases
+rmdir Releases /S /Q
+
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" GitMind.sln /t:rebuild /v:m /nologo
+
+echo.
+mkdir Releases >nul 2>&1
 copy GitMind\bin\Debug\GitMind.exe Releases\GitMindSetup.exe /Y 
 
+PowerShell -Command "& {(Get-Item Releases\GitMindSetup.exe).VersionInfo.FILEVERSION }" > Releases\version.txt
+echo.
+echo GitMindSetup.exe version:
+type Releases\version.txt 
+
+echo.
+echo.
 pause
