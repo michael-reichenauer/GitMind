3e936dc92ab8719e74a8ee2b9e205a2f1d3fb77b
Michael Reichenauer <michael.reichenauer@gmail.com>
2016-07-16 10:46:45 +0200

    Moved windows control (minimize, maximize and close buttons to a seperate user control

---
+++ GitMind/WindowControlButtons.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace GitMind
+{
+	/// <summary>
+	/// Interaction logic for WindowControlButtons.xaml
+	/// </summary>
+	public partial class WindowControlButtons : UserControl
+	{
+		public WindowControlButtons()
+		{
+			InitializeComponent();
+		}
+	}
+}
