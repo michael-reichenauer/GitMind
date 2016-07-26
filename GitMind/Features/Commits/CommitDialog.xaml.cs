585ae07b8e86d95f6fdbf10fdc476f402a715a00
Michael Reichenauer <michael.reichenauer@gmail.com>
2016-07-26 16:21:24 +0200

    Fixed ok, cancel and escape

--- GitMind/Features/Commits/CommitDialog.xaml.cs
+++ GitMind/Features/Commits/CommitDialog.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
 
 namespace GitMind.Features.Commits
 {
@@ -22,24 +11,7 @@ namespace GitMind.Features.Commits
 		public CommitDialog()
 		{
 			InitializeComponent();
-		}
-
-
-		private void OKButton_Click(object sender, RoutedEventArgs e)
-		{
-			DialogResult = true;
-		}
-
-
-		private void CancelButton_Click(object sender, RoutedEventArgs e)
-		{
-			DialogResult = false;
-		}
-
-
-		private void Exscape_Clicked(object sender, EventArgs e)
-		{
-			DialogResult = false;
+			DataContext = new CommitDialogViewModel();
 		}
 	}
 }
--- GitMind/Features/Commits/CommitDialogViewModel.cs
+++ GitMind/Features/Commits/CommitDialogViewModel.cs
@@ -1,10 +1,16 @@
-﻿using GitMind.Utils.UI;
+﻿using System.Windows;
+using System.Windows.Input;
+using GitMind.Utils.UI;
 
 
 namespace GitMind.Features.Commits
 {
 	internal class CommitDialogViewModel : ViewModel
 	{
-		
+		public ICommand OkCommand { get; } = new Command<Window>(w => w.DialogResult = true);
+
+		public ICommand CancelCommand { get; } = new Command<Window>(w => w.DialogResult = false);
+
+
 	}
 }
