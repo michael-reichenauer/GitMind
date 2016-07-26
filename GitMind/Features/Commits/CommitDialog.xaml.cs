e7d461663cda21aaf132a362adb28f30949be28e
Michael Reichenauer <michael.reichenauer@gmail.com>
2016-07-26 10:18:44 +0200

    Added empty Commit dialog

---
+++ GitMind/Features/Commits/CommitDialog.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GitMind.Features.Commits
+{
+	/// <summary>
+	/// Interaction logic for CommitDialog.xaml
+	/// </summary>
+	public partial class CommitDialog : Window
+	{
+		public CommitDialog()
+		{
+			InitializeComponent();
+		}
+
+
+		private void OKButton_Click(object sender, RoutedEventArgs e)
+		{
+			DialogResult = true;
+		}
+
+
+		private void CancelButton_Click(object sender, RoutedEventArgs e)
+		{
+			DialogResult = false;
+		}
+	}
+}
