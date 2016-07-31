d2b02775a0e97606207774fa2b48d2cd784fa162
Michael Reichenauer <michael.reichenauer@gmail.com>
2016-07-31 19:04:53 +0200

    Enable read cached repository

--- GitMind/GitModel/Private/CacheService.cs
+++ GitMind/GitModel/Private/CacheService.cs
@@ -21,15 +21,15 @@ namespace GitMind.GitModel.Private
 
 		public async Task<MRepository> TryGetRepositoryAsync(string gitRepositoryPath)
 		{
-			await Task.Yield();
-			return null;
-			//MRepository repository = await TryReadRepositoryAsync(gitRepositoryPath);
-			//if (repository != null)
-			//{
-			//	repository.CommitsFiles = new CommitsFiles();
-			//}
+			//await Task.Yield();
+			//return null;
+			MRepository repository = await TryReadRepositoryAsync(gitRepositoryPath);
+			if (repository != null)
+			{
+				repository.CommitsFiles = new CommitsFiles();
+			}
 
-			//return repository;
+			return repository;
 		}
 
 
