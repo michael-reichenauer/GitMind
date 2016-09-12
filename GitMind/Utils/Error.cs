e38e31be6e52255b5ba773702df4ee2efb8342e5
Michael Reichenauer <michael.reichenauer@gmail.com>
2016-09-12 20:58:51 +0200

    Cleanup Error type

--- GitMind/Utils/Error.cs
+++ GitMind/Utils/Error.cs
@@ -1,13 +1,11 @@
 using System;
-using System.CodeDom;
-using System.Diagnostics;
 
 
 namespace GitMind.Utils
 {
 	public class Error : Equatable<Error>
 	{
-		private static readonly Exception noneException = new Exception("none");
+		private static readonly Exception noException = new Exception("none");
 		private readonly Exception exception = null;
 
 		private Error(string message = null)
@@ -17,19 +15,23 @@ namespace GitMind.Utils
 
 		private Error(Exception exception, string message = null)
 		{
-			Message = message ?? "";
-			this.exception = exception;
-
 			if (message != null && exception != null)
 			{
 				Message = $"{message}, {exception.Message}";
+				this.exception = exception;
 			}
-			else if (exception != null)
+			else if (message == null && exception != null)
 			{
 				Message = exception.Message;
+				this.exception = exception;
+			}
+			else
+			{
+				Message = "Error";
+				this.exception = new Exception(Message);
 			}
 
-			if (exception != noneException)
+			if (exception != noException)
 			{
 				Log.Warn($"Error: {Message}");
 			}
@@ -42,9 +44,11 @@ namespace GitMind.Utils
 
 		public static Error From(Exception e, string message) => new Error(e, message);
 
-		public static Error From(string message = "") => new Error(message);
+		public static Error From(string message) => new Error(message);
 
-		public static Error None = new Error(noneException);
+		public static Error None = new Error(noException);
+
+		public static implicit operator Error(Exception e) => new Error(e);
 
 
 		public bool Is<T>()
@@ -56,36 +60,20 @@ namespace GitMind.Utils
 		protected override bool IsEqual(Error other)
 		{
 			if ((ReferenceEquals(this, None) && !ReferenceEquals(other, None))
-			    || !ReferenceEquals(this, None) && ReferenceEquals(other, None))
+					|| !ReferenceEquals(this, None) && ReferenceEquals(other, None))
 			{
 				return false;
 			}
 
-			return 
+			return
 				(exception == null && other.exception == null && GetType() == other.GetType())
 				|| other.GetType().IsInstanceOfType(this)
-				|| (GetType() == other.GetType() && exception != null && other.exception != null 
+				|| (GetType() == other.GetType() && exception != null && other.exception != null
 					&& other.exception.GetType().IsInstanceOfType(this));
 		}
 
 		protected override int GetHash() => 0;
 
 		public override string ToString() => Message;
-
-
-		//public Error With(string message)
-		//{
-		//	return new Error(errorCode, Message + " " + message);
-		//}
-
-		//public Error With(Error error)
-		//{
-		//	return new Error(errorCode, Message + " " + error);
-		//}
-
-		//public Error With(R result)
-		//{
-		//	return new Error(errorCode, Message + " " + result);
-		//}	
 	}
 }
