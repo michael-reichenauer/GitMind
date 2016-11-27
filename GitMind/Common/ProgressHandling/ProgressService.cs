b0d87e442c46aeedc48bd60691a7713886d03c2d
Michael Reichenauer <michael.reichenauer@gmail.com>
2016-11-27 08:33:22 +0100

    Add timing log to progress and busy indicator

--- GitMind/Common/ProgressHandling/ProgressService.cs
+++ GitMind/Common/ProgressHandling/ProgressService.cs
@@ -36,7 +36,7 @@ namespace GitMind.Common.ProgressHandling
 		{
 			Log.Debug($"Progress status: {text}");
 
-			ProgressImpl progress = new ProgressImpl(owner, text);
+			ProgressBox progress = new ProgressBox(owner, text);
 
 			progress.StartShowDialog();
 			currentProgress = progress;
@@ -44,26 +44,30 @@ namespace GitMind.Common.ProgressHandling
 		}
 
 
-		internal class ProgressImpl : Progress
+		internal class ProgressBox : Progress
 		{
 			private readonly ProgressDialog progressDialog;
-			private readonly TaskCompletionSource<bool> closeTask = new TaskCompletionSource<bool>();	
+			private readonly TaskCompletionSource<bool> closeTask = new TaskCompletionSource<bool>();
+			private Timing timing;
 
-			public ProgressImpl(WindowOwner owner, string text)
+			public ProgressBox(WindowOwner owner, string text)
 			{
+				timing = new Timing();
+				timing.Log($"Progress status: {text}");
 				progressDialog = new ProgressDialog(owner, text, closeTask.Task);
 			}
 	
 
 			public override void Dispose()
 			{
+				timing.Log("Progress status done");
 				closeTask.TrySetResult(true);
 			}
 
 
 			public override void SetText(string text)
 			{
-				Log.Debug($"Progress status: {text}");
+				timing.Log($"Progress status: {text}");
 				progressDialog.SetText(text);
 			}
 
