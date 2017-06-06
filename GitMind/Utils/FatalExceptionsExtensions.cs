3a39ce7559a42e2b6e5052a2d6a35b1b7cc3f7ad
Michael Reichenauer <michael.reichenauer@gmail.com>
2017-06-06 22:17:41 +0200

    Clean code

--- GitMind/Utils/FatalExceptionsExtensions.cs
+++ GitMind/Utils/FatalExceptionsExtensions.cs
@@ -7,21 +7,6 @@ using System.Runtime.InteropServices;
 
 namespace System
 {
-	internal class FatalExceptionEventArgs : EventArgs
-	{
-		public string Message { get; }
-
-		public Exception Exception { get; }
-
-
-		public FatalExceptionEventArgs(string message, Exception exception)
-		{
-			Message = message;
-			Exception = exception;
-		}
-	}
-
-
 	internal static class FatalExceptionsExtensions
 	{
 		private static readonly IReadOnlyList<Type> FatalTypes = new[]
