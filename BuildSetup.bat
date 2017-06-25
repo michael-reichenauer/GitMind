6e6a8ac9f1679da8fe0e2c404011d01870fe69ca
Michael Reichenauer <michael.reichenauer@gmail.com>
2017-06-25 20:05:15 +0200

    Update BuildSetup.bat script and include nuget.exe

--- BuildSetup.bat
+++ BuildSetup.bat
@@ -5,18 +5,45 @@ echo.
 del GitMindSetup.exe >nul 2>&1
 del version.txt >nul 2>&1
 
-call nuget restore GitMind.sln
-echo.
 
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" GitMind.sln /t:rebuild /v:m /nologo
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" (
+  set MSBUILD="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+)
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" (
+  set MSBUILD="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe"
+)
+if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" (
+  set MSBUILD="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+)
 
-echo.
-copy GitMind\bin\Debug\GitMind.exe GitMindSetup.exe /Y 
+if exist %MSBUILD% (
+  echo Using MSBuild: %MSBUILD%
+  echo.
 
-PowerShell -Command "& {(Get-Item GitMindSetup.exe).VersionInfo.FILEVERSION }" > version.txt
-echo.
-echo GitMindSetup.exe version:
-type version.txt 
+
+  echo Restore nuget packets ...
+  rem call %MSBUILD% /nologo /t:restore /v:m GitMind.sln
+  .\Binaries\nuget.exe restore GitMind.sln
+  echo.
+
+  echo Building ...
+  %MSBUILD% /t:rebuild /v:m /nologo GitMind.sln 
+  echo.
+
+  echo Copy to Setup file ...
+  copy GitMind\bin\Debug\GitMind.exe GitMindSetup.exe /Y 
+
+  echo Get built version...
+  PowerShell -Command "& {(Get-Item GitMindSetup.exe).VersionInfo.FILEVERSION }" > version.txt
+  echo.
+
+  echo GitMindSetup.exe version:
+  type version.txt 
+  
+) else (
+  echo.
+  echo Error: Failed to locate compatible msbuild.exe
+)
 
 echo.
 echo.
