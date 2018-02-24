57de4991eb85d26519b331d581427567f69ada70
Michael Reichenauer <michael.reichenauer@gmail.com>
2018-02-24 11:23:02 +0100

    Add missing files

---
+++ GitMindTest/AutoMocking/AutoMock.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Linq;
+using Autofac;
+using Autofac.Features.ResolveAnything;
+using GitMindTest.AutoMocking.Private;
+using Moq;
+
+
+namespace GitMindTest.AutoMocking
+{
+	public class AutoMock : IDisposable
+	{
+		private readonly Lazy<IContainer> container;
+		private readonly ContainerBuilder containerBuilder;
+		private readonly AutofacMoqSource autofacMoqSource;
+
+		private bool disposed;
+
+
+		public AutoMock()
+			: this(new ContainerBuilder())
+		{
+		}
+
+
+		public AutoMock(ContainerBuilder containerBuilder)
+		{
+			this.containerBuilder = containerBuilder;
+
+			containerBuilder.RegisterSource(new AnyConcreteTypeNotAlreadyRegisteredSource());
+			containerBuilder.RegisterSource(autofacMoqSource = new AutofacMoqSource());
+
+			container = new Lazy<IContainer>(() => containerBuilder.Build());
+		}
+
+		private ContainerBuilder ContainerBuilder
+		{
+			get
+			{
+				if (container.IsValueCreated)
+				{
+					throw new InvalidOperationException(
+						"Cannot register after first use of Resolve<T>() or Mock<T>()");
+				}
+
+				return containerBuilder;
+			}
+		}
+
+
+		public T Resolve<T>() => container.Value.Resolve<T>();
+
+
+		public T Resolve<TArg1, T>(TArg1 arg1) =>
+			container.Value.Resolve<Func<TArg1, T>>()(arg1);
+
+		public T Resolve<TArg1, TArg2, T>(TArg1 arg1, TArg2 arg2) =>
+			container.Value.Resolve<Func<TArg1, TArg2, T>>()(arg1, arg2);
+
+
+		public T Resolve<TArg1, TArg2, TArg3, T>(TArg1 arg1, TArg2 arg2, TArg3 arg3) =>
+			container.Value.Resolve<Func<TArg1, TArg2, TArg3, T>>()(arg1, arg2, arg3);
+
+
+		public Mock<T> Mock<T>()
+			where T : class
+		{
+			IMocked<T> obj = (IMocked<T>)Resolve<T>();
+			return obj.Mock;
+		}
+
+
+		public void VerifyAll() => container.Value.Resolve<MockRepository>().VerifyAll();
+
+
+		/// <summary>
+		/// Register the specified Mock and mocked instance to overide already 
+		/// registered type.
+		/// </summary>
+		public AutoMock RegisterMock<T>()
+			where T : class
+		{
+			Mock<T> mock = new Mock<T>(MockBehavior.Strict);
+
+			RegisterAsSingleInstance(ContainerBuilder, mock);
+			RegisterAsSingleInstance(ContainerBuilder, mock.Object);
+
+			return this;
+		}
+
+		public AutoMock RegisterType<T>()
+			where T : class
+		{
+			RegisterTypes(ContainerBuilder, new[] { typeof(T) });
+			return this;
+		}
+
+
+		public AutoMock RegisterSingleInstance<T>(T instance)
+			where T : class
+		{
+			ContainerBuilder.RegisterInstance(instance)
+				.As<T>()
+				.AsSelf()
+				.SingleInstance();
+
+			RegisterAsSingleInstance(ContainerBuilder, instance);
+			return this;
+		}
+
+		public AutoMock RegisterNamespaceOf<T>()
+		{
+			string nameSpace = typeof(T).Namespace;
+
+			var types = typeof(T).Assembly.GetTypes().Where(type =>
+				type.Namespace != null &&
+				nameSpace != null &&
+				type.Namespace.StartsWith(nameSpace, StringComparison.Ordinal)).ToArray();
+
+			RegisterTypes(ContainerBuilder, types);
+
+			return this;
+		}
+
+		public AutoMock RegisterAssemblyOf<T>()
+		{
+			RegisterTypes(ContainerBuilder, typeof(T).Assembly.GetTypes());
+			return this;
+		}
+
+
+		public void Dispose()
+		{
+			autofacMoqSource.Dispose();
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+
+		private void Dispose(bool disposing)
+		{
+			if (!disposed)
+			{
+				if (disposing)
+				{
+					container.Value.Dispose();
+				}
+
+				disposed = true;
+			}
+		}
+
+
+		private static void RegisterAsSingleInstance(ContainerBuilder builder, object instance)
+		{
+			builder.RegisterInstance(instance)
+				.AsImplementedInterfaces()
+				.AsSelf()
+				.SingleInstance();
+		}
+
+
+		private static void RegisterTypes(ContainerBuilder builder, Type[] types)
+		{
+			builder.RegisterTypes(types)
+				.Where(t => !IsSingleInstance(t))
+				.AsImplementedInterfaces()
+				.AsSelf();
+
+			builder.RegisterTypes(types)
+				.Where(IsSingleInstance)
+				.AsImplementedInterfaces()
+				.AsSelf()
+				.SingleInstance();
+		}
+
+
+		private static bool IsSingleInstance(Type type) =>
+			type.GetCustomAttributes(true)
+			.Any(t => t.GetType().Name == "SingleInstanceAttribute");
+	}
+}
---
+++ GitMindTest/AutoMocking/Private/AutofacMoqSource.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using Autofac.Core;
+using Autofac.Core.Activators.Delegate;
+using Autofac.Core.Lifetime;
+using Autofac.Core.Registration;
+
+
+namespace GitMindTest.AutoMocking.Private
+{
+	internal class AutofacMoqSource : IRegistrationSource, IDisposable
+	{
+		private readonly Autofac.Extras.Moq.AutoMock autofacAutoMock;
+
+
+		public AutofacMoqSource()
+		{
+			autofacAutoMock = Autofac.Extras.Moq.AutoMock.GetStrict();
+		}
+
+
+		public bool IsAdapterForIndividualComponents => false;
+
+
+		public IEnumerable<IComponentRegistration> RegistrationsFor(
+			Service service,
+			Func<Service, IEnumerable<IComponentRegistration>> registrationAccessor)
+		{
+			var typedService = service as TypedService;
+
+			if (typedService == null ||
+					!CanMockService(typedService) ||
+					registrationAccessor(service).Any())
+			{
+				return Enumerable.Empty<IComponentRegistration>();
+			}
+
+			var registration = new ComponentRegistration(
+				Guid.NewGuid(),
+				DelegateMockActivatorFor(typedService),
+				new CurrentScopeLifetime(),
+				InstanceSharing.Shared, InstanceOwnership.OwnedByLifetimeScope,
+				new[] { service },
+				new Dictionary<string, object>());
+
+			return new IComponentRegistration[] { registration };
+		}
+
+
+		public void Dispose()
+		{
+			autofacAutoMock.Dispose();
+		}
+
+
+		private DelegateActivator DelegateMockActivatorFor(IServiceWithType typedService)
+		{
+			return new DelegateActivator(
+				typedService.ServiceType,
+				(c, p) => autofacAutoMock.Container.Resolve(typedService.ServiceType));
+		}
+
+		private static bool CanMockService(IServiceWithType typedService)
+		{
+			return !typeof(IStartable).IsAssignableFrom(
+				typedService.ServiceType) && !IsConcrete(typedService.ServiceType);
+		}
+
+		private static bool IsConcrete(Type serviceType)
+		{
+			return serviceType.IsClass && !serviceType.IsAbstract && !serviceType.IsInterface;
+		}
+	}
+}
