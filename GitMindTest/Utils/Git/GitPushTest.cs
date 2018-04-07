33e772ae24faa4af4d9488ae6af2d01e53c0fbf5
Michael Reichenauer <michael.reichenauer@gmail.com>
2018-04-07 11:34:15 +0800

    Add push test

--- GitMindTest/Utils/Git/GitPushTest.cs
+++ GitMindTest/Utils/Git/GitPushTest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using GitMind.Utils;
 using GitMind.Utils.Git;
 using GitMindTest.Utils.Git.Private;
 using NUnit.Framework;
@@ -14,20 +15,44 @@ namespace GitMindTest.Utils.Git
 		{
 			await CloneRepoAsync();
 
+			// After cloning an empty repo, there are no branches until first commit
+			branches = await GetBranchesAsync();
+			Assert.AreEqual(0, branches.Count);
+
+			// First commit
 			FileWrite("file1.txt", "text 1");
 			await CommitAllChangesAsync("Message 1");
 
-			var branches = await GetBranchesAsync();
+			// After first commit, but before first push, only the local master exist,
+			// The remote master has not yet been created and is thus "missing"
+			branches = await GetBranchesAsync();
+			Assert.AreEqual(1, branches.Count);
+			Assert.AreEqual(true, branches[0].IsRemoteMissing);
 
-			//R result = await gitCmd.PushAsync(ct);
-			//Assert.IsTrue(result.IsOk);
+			// First push
+			R result = await gitCmd.PushAsync(ct);
+			Assert.AreEqual(true, result.IsOk);
 
-			//status = await GetStatusAsync();
+			// Now there are two branches one local and one remote
+			branches = await GetBranchesAsync();
+			Assert.AreEqual(2, branches.Count);
+			Assert.AreEqual(false, branches[0].IsRemoteMissing);
 
-			//FileWrite("file1.txt", "text 2");
-			//await CommitAllChangesAsync("Message 2");
+			// Second commit
+			FileWrite("file1.txt", "text 2");
+			await CommitAllChangesAsync("Message 2");
 
-			//status = await GetStatusAsync();
+			// Now local branch is 1 ahead
+			branches = await GetBranchesAsync();
+			Assert.AreEqual(1, branches[0].AheadCount);
+
+			// Second push
+			result = await gitCmd.PushAsync(ct);
+			Assert.AreEqual(true, result.IsOk);
+
+			// Branch is insync
+			branches = await GetBranchesAsync();
+			Assert.AreEqual(0, branches[0].AheadCount);
 		}
 	}
 }
