f6263f635f4173c67d84222fad0d679b3aef5bcb
Michael Reichenauer <michael.reichenauer@gmail.com>
2018-04-23 08:27:48 +0800

    Adjust switch

--- GitMind/Features/Branches/Private/BranchService.cs
+++ GitMind/Features/Branches/Private/BranchService.cs
@@ -167,7 +167,7 @@ namespace GitMind.Features.Branches.Private
 
 		public async Task SwitchBranchAsync(Branch branch)
 		{
-			using (statusService.PauseStatusNotifications())
+			using (statusService.PauseStatusNotifications(Refresh.Status))
 			using (progress.ShowDialog($"Switching to branch {branch.Name} ..."))
 			{
 				R result = await gitBranchService.SwitchToBranchAsync(
