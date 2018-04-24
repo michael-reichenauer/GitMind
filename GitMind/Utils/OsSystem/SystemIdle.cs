1e939dc6e7112aef32fc794a4147548aaed7face
Michael Reichenauer <michael.reichenauer@gmail.com>
2018-04-24 08:20:37 +0800

    Add support for get SystemIdle time

---
+++ GitMind/Utils/OsSystem/SystemIdle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+
+namespace GitMind.Utils.OsSystem
+{
+	public class SystemIdle
+	{
+		public static void TriggerOnIdle(TimeSpan idleTime, Action action, CancellationToken ct)
+		{
+			void CheckIdle(object state)
+			{
+				if (DateTime.UtcNow - GetLastInputUtcTime() > idleTime)
+				{
+					action();
+				}
+			}
+
+			Timer timer = new Timer(CheckIdle);
+			timer.Change(TimeSpan.FromMinutes(1), TimeSpan.FromMinutes(1));
+			ct.Register(() => timer.Dispose());
+		}
+
+
+		public static TimeSpan GetLastInputIdleTimeSpan() => DateTime.UtcNow - GetLastInputUtcTime();
+
+
+		public static DateTime GetLastInputUtcTime()
+		{
+			var lastInputInfo = new Native.LASTINPUTINFO();
+			lastInputInfo.cbSize = (uint)Marshal.SizeOf(lastInputInfo);
+
+			Native.GetLastInputInfo(ref lastInputInfo);
+
+			DateTime lastInput = DateTime.UtcNow.AddMilliseconds(
+				-(Environment.TickCount - lastInputInfo.dwTime));
+			return lastInput;
+		}
+
+
+
+		private static class Native
+		{
+			[DllImport("User32.dll")]
+			public static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+
+			[StructLayout(LayoutKind.Sequential)]
+			internal struct LASTINPUTINFO
+			{
+				public uint cbSize;
+				public uint dwTime;
+			}
+		}
+	}
+}
---
+++ GitMindTest/Utils/OsSystem/SystemIdleTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using GitMind.Utils;
+using GitMind.Utils.OsSystem;
+using NUnit.Framework;
+
+
+namespace GitMindTest.Utils.OsSystem
+{
+	[TestFixture]
+	public class SystemIdleTest
+	{
+		[Test, Explicit]
+		public void TestIdle()
+		{
+			for (int i = 0; i < 30; i++)
+			{
+				Log.Debug($"Time: {SystemIdle.GetLastInputIdleTimeSpan()}");
+				Thread.Sleep(1000);
+			}
+		}
+	}
+}
