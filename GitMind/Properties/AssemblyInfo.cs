c66119f5da56cba5417eb2f2cc5d7b3adcb49adf
Michael Reichenauer <michael.reichenauer@gmail.com>
2018-04-27 11:00:27 +0800

    Version 0.136

--- GitMind/Properties/AssemblyInfo.cs
+++ GitMind/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.135.*")]
+[assembly: AssemblyVersion("0.136.*")]
 
 [assembly: InternalsVisibleTo("GitMindTest")]
 
