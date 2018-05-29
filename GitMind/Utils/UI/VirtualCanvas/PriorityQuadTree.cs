81e1a9f38c0af8424f6e330c219422a14bf059cf
Michael Reichenauer <michael.reichenauer@gmail.com>
2018-05-29 13:53:38 +0200

    Fix compiler warnings

--- GitMind/Utils/UI/VirtualCanvas/PriorityQuadTree.cs
+++ GitMind/Utils/UI/VirtualCanvas/PriorityQuadTree.cs
@@ -20,7 +20,7 @@ namespace GitMind.Utils.UI.VirtualCanvas
 	public class PriorityQuadTree<T> : IEnumerable<T>
 	{
 		/// <summary>
-		/// Each node stored in the tree has a position, width & height.        
+		/// Each node stored in the tree has a position, width height.        
 		/// </summary>
 		private class QuadNode
 		{
